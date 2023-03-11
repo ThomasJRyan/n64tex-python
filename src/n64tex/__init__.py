a25ed0e1d0802cee30397a79c27ad730bd08a1f7
Thomas Ryan <tomryan_8_1997@hotmail.com>
2023-03-11 17:08:43 -0500

    Added ability to save to bytes

--- src/n64tex/__init__.py
+++ src/n64tex/__init__.py
@@ -11,8 +11,8 @@ def cli() -> None:
 
     parser.add_argument("filepath", help="Path to file to convert")
     
-    parser.add_argument("--width", type=int, help="Width of image")
-    parser.add_argument("--height", type=int, help="Height of image")
+    parser.add_argument("--width", type=int, help="Width of image. Defaults to 64", default=64)
+    parser.add_argument("--height", type=int, help="Height of image. Defaults to 64", default=64)
 
     parser.add_argument(
         "input_format",
@@ -44,13 +44,13 @@ def cli() -> None:
             "ci8",
             "rgba",
             "rgba5551",
-            "bytes",
         ],
     )
     
     parser.add_argument("--palette", help="File containing palette information. Only required for CI4/CI8 input format")
 
     parser.add_argument("--output_file", "-o", help="Output file name", type=str)
+    parser.add_argument("--write_bytes", "-b", action="store_true", help="Write a bytes file")
 
     args = parser.parse_args()
 
@@ -79,16 +79,25 @@ def cli() -> None:
     cls = Formats[input_format].value
     try:
         image = Image.open(filepath)
-        width = image.width or args.width or 64
-        height = image.height or args.height or 64
+        width = image.width or args.width
+        height = image.height or args.height
         obj = cls.from_image(image, width, height)
     except UnidentifiedImageError:
         with open(filepath, 'rb') as fil:
             image = fil.read()
-        width = args.width or 64
-        height = args.height or 64
+        width = args.width
+        height = args.height
         obj = cls.from_bytes(image, width, height, palette_data)
     converted_obj = obj.convert_to(Formats[output_format].value)
+        
+    # Handle writing to bytes
+    if args.write_bytes:
+        with open(output_file.stem, 'wb') as fil:
+            fil.write(converted_obj.to_bytes())
+        if converted_obj.palette is not None:
+            palette_path = output_file.parent / f'palette_{output_file.name}'
+            with open(palette_path.stem, 'wb') as fil:
+                fil.write(converted_obj.palette.astype('>u2').tobytes())
     
     # Save image
     converted_obj.save(output_file)
