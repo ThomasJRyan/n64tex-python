Fixed issue where empty pixel would get overflow data

src/n64tex/formats/ci4.py
@@ -64,8 +64,8 @@ class CI4Image(BaseImage):
         # Image pointers
         split_bytes = lambda x: ((x & 0xF0) >> 4, x & 0x0F)
         data_array = np.frombuffer(raw_bytes, dtype=">u1")
-        data_array = np.ravel(np.column_stack(split_bytes(data_array)))
-        data_array = np.resize(data_array, (height, width))
+        data_array = np.array(np.ravel(np.column_stack(split_bytes(data_array))))
+        data_array.resize((height, width), refcheck=False)
         
         # Image palette
         assert palette_bytes, "CI4 images require a palette to function"

src/n64tex/formats/ci8.py
@@ -62,8 +62,11 @@ class CI8Image(BaseImage):
             CI8Image: CI8Image object
         """
         # Image pointers
+        if len(raw_bytes) == 2720:
+            import pudb; pu.db
         data_array = np.frombuffer(raw_bytes, dtype=">u1")
-        data_array = np.resize(data_array, (height, width))
+        data_array = np.array(data_array)
+        data_array.resize((height, width), refcheck=False)
         
         # Image palette
         assert palette_bytes, "CI8 images require a palette to function"

src/n64tex/formats/i4a.py
@@ -35,8 +35,8 @@ class I4AImage(BaseImage):
         """
         split_bytes = lambda x: ((x & 0xF0) >> 4, x & 0x0F)
         data_array = np.frombuffer(raw_bytes, dtype=">u1")
-        data_array = np.ravel(np.column_stack(split_bytes(data_array)))
-        data_array = np.resize(data_array, (height, width))
+        data_array = np.array(np.ravel(np.column_stack(split_bytes(data_array))))
+        data_array.resize((height, width), refcheck=False)
         return cls(data_array, width, height)
 
     def to_rgba(self, colour: tuple[int, int, int] = (255, 255, 255)) -> "RGBAImage":

src/n64tex/formats/i8a.py
@@ -34,7 +34,8 @@ class I8AImage(BaseImage):
             I8AImage: I8AImage object
         """
         data_array = np.frombuffer(raw_bytes, dtype=">u1")
-        data_array = np.resize(data_array, (height, width))
+        data_array = np.array(data_array)
+        data_array.resize((height, width), refcheck=False)
         return cls(data_array, width, height)
 
     def to_rgba(self, colour: tuple[int, int, int] = (255, 255, 255)) -> "RGBAImage":

src/n64tex/formats/rgba.py
@@ -33,7 +33,8 @@ class RGBAImage(BaseImage):
             RGBAImage: RGBAImage object
         """
         data_array = np.frombuffer(raw_bytes, dtype=">u1")
-        data_array = np.resize(data_array, (height, width, 4)).astype(">u1")
+        data_array = np.array(data_array)
+        data_array.resize((height, width, 4), refcheck=False)
         return cls(data_array, width, height)
 
     def convert_to(self, cls: T) -> T:

src/n64tex/formats/rgba5551.py
@@ -33,7 +33,8 @@ class RGBA5551Image(BaseImage):
             RGBA5551Image: RGBA5551Image object
         """
         data_array = np.frombuffer(raw_bytes, dtype=">u2")
-        data_array = np.resize(data_array, (height, width)).astype(">u2")
+        data_array = np.array(data_array)
+        data_array.resize((height, width), refcheck=False)
         return cls(data_array, width, height)
 
     def to_rgba(self) -> "RGBAImage":